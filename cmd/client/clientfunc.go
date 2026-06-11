a40828676d293a63cc6aaaeee200f47a351abbd9
JoseCarlitosGordo <jose.gordo0806@gmail.com>
2026-06-12 01:48:40 +0930

    msg system works well enough. Now i gotta figure out how to store msg logs persistently

--- cmd/client/clientfunc.go
+++ cmd/client/clientfunc.go
@@ -3,34 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"net"
 	"os"
 )
 
 func sendMessage() string {
 	msgScanner := bufio.NewScanner(os.Stdin)
-	fmt.Printf("(Type 'exit()' to close this app) Your Message: ")
+	fmt.Printf("\n(Type 'exit()' to close this app) \nYour Message: ")
 	msgScanner.Scan()
 	message := msgScanner.Text()
 	return message
-}
-
-func msgSender() {
-	//send over the tcp protocol
-	conn, err := net.Dial("tcp", "localhost:8080")
-	if err != nil {
-		fmt.Printf("%s", err.Error())
-	}
-	defer conn.Close()
-	for {
-
-		msgToSend := sendMessage()
-		if msgToSend == "exit()" {
-			return
-		}
-		fmt.Printf("%s", msgToSend)
-		fmt.Fprintf(conn, "%s", msgToSend)
-
-	}
-
 }
--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"net"
+)
+
 func main() {
-	go msgSender()
+	//send over the tcp protocol
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Printf("%s", err.Error())
+		return
+	}
+	defer conn.Close()
+	for {
+		msgToSend := sendMessage()
+		if msgToSend == "exit()" {
+			return
+		}
+		_, err := fmt.Fprintf(conn, "%s", msgToSend)
+		if err != nil {
+			fmt.Printf("The server has stopped working unexpectedly...")
+			return
+		}
+
+	}
 }
